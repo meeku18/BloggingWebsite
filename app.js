bc84f715757895309ef6a06f504042854f3a04e3
yoyashsingg <105302581+yoyashsingg@users.noreply.github.com>
2022-11-13 12:01:26 +0530

    final commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "We made a website as a solution to the problem 1. Our website GalCare is a blogging site, where professionals share blogs on different problems women are hesitant to talk about and may prove fatal for them at a later stage, hence spreading awareness among people. We have provided a facility where women can consult with an experienced doctor and talk about their problems more freely. The idea is to tie up with experts in different fields, such that they get an one stop solution for all their problems. We aim to provide a service where women can get the required medication for their problems. A FAQs section is included which provides some YouTube links to the most common problems women face, and we aim to update it on regular basis depending on the feedback we get. We also thought of including a feedback section to get feedback from our users and to know which areas to work on.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -16,8 +16,13 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use('/public/images/',express.static('./public/images'));
 
-mongoose.connect("mongodb://localhost:27017/blogDB",)
+mongoose.connect("mongodb+srv://admin-yash:test-1234@cluster0.wulzegj.mongodb.net/todolistDB");
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server started");
+});
 
 const postSchema = {
   title : String,
@@ -34,7 +39,7 @@ app.get("/", function(req, res){
       posts: posts
       });
   });
-
+  
 });
 
 
@@ -76,6 +81,4 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+
